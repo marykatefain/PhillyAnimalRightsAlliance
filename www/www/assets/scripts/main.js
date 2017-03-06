979149620ec6cbe090d84d8429a10979ed132804
Mary Kate Fain <mkfain1@gmail.com>
2017-03-05 22:13:46 -0500

    cleanup

--- www/www/assets/scripts/main.js
+++
@@ -1,7 +0,0 @@
-function showDiv() {
-  document.getElementById('hidden_form').style.display = "block";
-}
-
-function hideDiv() {
-  document.getElementById('hidden_form').style.display = "none";
-}
