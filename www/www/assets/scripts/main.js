2d8698961c61298a268691f1c84771c39b345605
Mary Kate Fain <mkfain1@gmail.com>
2017-03-05 21:49:36 -0500

    renamed nunjucks file extensions to be standard njk

---
+++ www/www/assets/scripts/main.js
@@ -0,0 +1,7 @@
+function showDiv() {
+  document.getElementById('hidden_form').style.display = "block";
+}
+
+function hideDiv() {
+  document.getElementById('hidden_form').style.display = "none";
+}
