34a5686e5464bf94973716d559f39e7ddf9cb578
Mary Kate Fain <mkfain1@gmail.com>
2017-04-13 16:20:26 -0400

    printing html test working

--- www/assets/scripts/main.js
+++ www/assets/scripts/main.js
@@ -8,33 +8,39 @@ function hideDiv() {
 
 
 var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
 
-var data = JSON.parse(this.responseText);
+    var data = JSON.parse(this.responseText);
 
-var entries = data.feed.entry;
+    var entries = data.feed.entry;
 
-var events = [];
+    var events = [];
 
-for (var i=0; i<entries.length; i++) {
-  var entry = entries[i];
-  events.push({
-    "id": entry.gsx$id.$t,
-    "title": entry.gsx$titleofevent.$t,
-    "date": entry.gsx$date.$t,
-    "start": entry.gsx$starttime.$t,
-    "end": entry.gsx$endtime.$t,
-    "location": entry.gsx$location.$t,
-    "class": entry.gsx$typeofevent.$t,
-    "url": entry.gsx$date.$t,
-    "contact": entry.gsx$contact.$t,
-    "details": entry.gsx$details.$t
-  });
-}
-console.log(events);
+    for (var i=0; i<entries.length; i++) {
+      var entry = entries[i];
+      events.push({
+        "id": entry.gsx$id.$t,
+        "title": entry.gsx$titleofevent.$t,
+        "date": entry.gsx$date.$t,
+        "start": entry.gsx$starttime.$t,
+        "end": entry.gsx$endtime.$t,
+        "location": entry.gsx$location.$t,
+        "class": entry.gsx$typeofevent.$t,
+        "url": entry.gsx$date.$t,
+        "contact": entry.gsx$contact.$t,
+        "details": entry.gsx$details.$t
+      });
     }
-    
-  };
-  xhttp.open("GET", "https://spreadsheets.google.com/feeds/list/1nRQfilLPP1Cm2nO2RP9vnmVHRbDWTrl5bBILJvQC2QA/1/public/values?alt=json", true);
-  xhttp.send();
+    console.log(events);
+
+    var x = "testing 123";
+
+    document.getElementById('calendar').innerHTML = x;
+
+
+  }
+
+};
+xhttp.open("GET", "https://spreadsheets.google.com/feeds/list/1nRQfilLPP1Cm2nO2RP9vnmVHRbDWTrl5bBILJvQC2QA/1/public/values?alt=json", true);
+xhttp.send();
