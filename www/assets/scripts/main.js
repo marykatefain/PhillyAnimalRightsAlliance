6e369d31c520018b7bc162789006ed3649779ee8
Mary Kate Fain <mkfain1@gmail.com>
2017-04-13 12:16:09 -0400

    trying to create event lists from ajax

--- www/assets/scripts/main.js
+++ www/assets/scripts/main.js
@@ -5,3 +5,22 @@ function showDiv() {
 function hideDiv() {
   document.getElementById('hidden_form').style.display = "none";
 }
+
+
+var response = jQuery.ajax('https://spreadsheets.google.com/feeds/list/1nRQfilLPP1Cm2nO2RP9vnmVHRbDWTrl5bBILJvQC2QA/1/public/values?alt=json');
+
+var data = JSON.parse(response.responseText);
+
+var entries = data.feed.entry;
+
+var events = [];
+
+for (var i=0; i<entries.length; i++) {
+  var entry = entries[i];
+  events.push({
+    "id": entry.gsx$id.$t,
+    "title": entry.gsx$titleofevent.$t,
+    "date": entry.gsx$starttime.$t,
+    // TODO: fill this out
+  });
+}``
